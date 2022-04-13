a280552054e4a83eb38305f17dfc0cc496f8449f
adofsauron <adofsauron@163.com>
2022-04-13 19:27:58 +0800

    fix trunk

--- commit-trunk.bat
+++ commit-trunk.bat
@@ -10,5 +10,13 @@ git commit -m "fix trunk"
 
 git push -u origin trunk
 
+git checkout master
+
+git merge trunk
+
+git push -u origin master
+
+git checkout trunk
+
 pause
 
--- redis-operator-1.1.1/run_ctr.sh
+++ redis-operator-1.1.1/run_ctr.sh
@@ -3,4 +3,4 @@
 export KUBERNETES_SERVICE_HOST="127.0.0.1"
 export KUBERNETES_SERVICE_PORT="6443"
 
-./ctr -development=true
+./ctr -development=true -debug=true
